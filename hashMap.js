566cc72a35ea078ebffd485ba485280504b39b10
Aung Khant Paing <akpaing99@gmail.com>
2024-11-08 16:13:34 +0630

    added new method to hashMap -added get method -growth the buckets

--- hashMap.js
+++ hashMap.js
@@ -1,22 +1,18 @@
 import { LinkedList } from "./linkedList.js";
 export class HashMap {
+  #loadFactor = 0.8;
+  #entriesCount = 0;
+  #capacity = 16;
   constructor() {
     this.buckets = [];
-    for (let i = 0; i < 16; i++) {
-      this.buckets.push(new LinkedList());
+    this.#populateBuckets(this.#capacity, this.buckets);
+  }
+  #populateBuckets(index, arr) {
+    for (let i = 0; i < index; i++) {
+      arr.push(new LinkedList());
     }
   }
-  set(key, val) {
-    let index = this.hash(key) % 16;
-    let bucket = this.buckets[index];
-    if (bucket.contain(key)) {
-      let bucketIndex = bucket.find(key);
-      bucket.replaceAt(bucketIndex, key, val);
-      return;
-    }
-    bucket.append(key, val);
-  }
-  hash(key) {
+  #hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
@@ -25,5 +21,43 @@ export class HashMap {
     }
 
     return hashCode;
+  }
+  #growthBuckets(size) {
+    let tmpArr = [];
+    this.#populateBuckets(size, tmpArr);
+    this.buckets.forEach((list) => {
+      if (list.size() != 0) {
+        let tmp = list.head;
+        while (tmp !== null) {
+          let index = this.#hash(tmp.key) % this.#capacity;
+          tmpArr[index].append(tmp.key, tmp.value);
+          tmp = tmp.next;
+        }
+      }
+    });
+    this.buckets = tmpArr;
+  }
+  set(key, val) {
+    if (this.#capacity * this.#loadFactor < this.#entriesCount) {
+      this.#capacity = this.#capacity * 2;
+      this.#growthBuckets(this.#capacity);
+    }
+    let index = this.#hash(key) % this.#capacity;
+    let bucket = this.buckets[index];
+    if (bucket.contain(key)) {
+      let bucketIndex = bucket.find(key);
+      bucket.replaceAt(bucketIndex, key, val);
+      return;
+    }
+    bucket.append(key, val);
+    this.#entriesCount++;
+  }
+  get(key) {
+    let index = this.#hash(key) % this.#capacity;
+    let tmp = this.buckets[index].head;
+    if (tmp !== null) {
+      while (key !== tmp.key && tmp.next != null) tmp = tmp.next;
+      if (key == tmp.key) return tmp.value;
+    } else return null;
   }
 }
